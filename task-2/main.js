539856174c473194d9e21d134c9f29d2adedbf50
denismazanov <82777275+denismazanov@users.noreply.github.com>
2022-06-30 23:58:21 +0300

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -5,5 +5,13 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+  let l = [9,5,-7,-7,5,-9,-4]
+  let count = 0;
+  for(let i =1;i<l.length-1;i++){
+      if(l[i-1]<l[i]&& l[i]>l[i+1]){
+          count++
+      }
+  }
+  console.log(count)
   
 });
