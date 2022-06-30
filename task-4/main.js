babd3b9cac21ee31b8759a202a09d9e4d5147d70
denismazanov <82777275+denismazanov@users.noreply.github.com>
2022-06-30 23:59:26 +0300

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -5,5 +5,14 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
-  
+  let array = [0,1,-2,1,0,0,3];
+  let array2 = [];
+  for(let i=0;i<array.length;i++){
+          if(!array2.includes(array[i])){
+              array2.push(array[i])}
+      }
+
+
+
+  console.log(array2)
 });
