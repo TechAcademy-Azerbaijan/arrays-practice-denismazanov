473dca1ebe474552b1bf95dfe3a70e83092c65a8
denismazanov <82777275+denismazanov@users.noreply.github.com>
2022-06-30 23:57:40 +0300

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -5,5 +5,20 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+      let a = [1,6,2,6,3]
+
+      let sum = 0;
+
+      for(let i=0;i<a.length;i++){
+          sum+=a[i]
+
+      }
+      let count = 0;
+      for(let m = 0;m<a.length;m++){
+          if(a[m]>sum/a.length){
+              count++
+          }
+      }
+      console.log(count)
   
 });
