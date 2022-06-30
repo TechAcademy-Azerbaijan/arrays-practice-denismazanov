6d6cdba70996305d21b7ea8377958fcff2cd416d
denismazanov <82777275+denismazanov@users.noreply.github.com>
2022-06-30 23:59:00 +0300

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -5,5 +5,30 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+  let array = [3,5,-7,7,5,-9,-4]
+  let l = [...array]
+
+  let max = 0;  // 7
+  let min = l[0];  // -9
+
+  for(let i=0;i<l.length;i++){
+      if(l[i]>max){
+          max = l[i]
+      }
+      if(l[i]<min){
+          min = l[i]
+      }
+  }
+
+  for(let m = 0;m<l.length;m++){
+      if(l[m]===max){
+          l[m]=min
+      }
+      else if(l[m]===min){
+          l[m]=max
+      }
+  }
+
+  console.log(l)
   
 });
